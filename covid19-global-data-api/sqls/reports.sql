ad4fae5931176f0b8b9c86a0036c955ddfc8cd3e
ChihYu Yeh <chihyu.jimmy.yeh@gmail.com>
2023-07-06 18:40:58 +0800

    chore: refactor

--- covid19-global-data-api/sqls/reports.sql
+++ covid19-global-data-api/sqls/reports.sql
@@ -1,14 +1,12 @@
+-- set country_codes variable given query parameter
 {% set country_codes = context.params.country_code %}
 
-{% if country_codes %}
-    SELECT * FROM read_csv_auto('WHO-COVID-19-global-data.csv')
-    WHERE 
-        Date_reported >= {{ context.params.start_date | is_required }} AND 
-        Date_reported <= {{ context.params.end_date | is_required }} AND
-        Country_code IN (SELECT UNNEST(string_split({{ country_codes }}, ',')))
-{% else %}
-    SELECT * FROM read_csv_auto('WHO-COVID-19-global-data.csv')
-    WHERE
-        Date_reported >= {{ context.params.start_date | is_required }} AND 
-        Date_reported <= {{ context.params.end_date | is_required }}
-{% endif %}
+-- get a list of COVID-19 reports given a date range and country codes(optional)
+SELECT * FROM read_csv_auto('WHO-COVID-19-global-data.csv')
+WHERE 
+    Date_reported >= {{ context.params.start_date | is_required }} AND 
+    Date_reported <= {{ context.params.end_date | is_required }}
+
+    {% if country_codes %}
+    AND Country_code IN (SELECT UNNEST(string_split({{ country_codes }}, ',')))
+    {% endif %}
